9031c01daf37ecd215abec4875534691ab07cc8f
教会 <141677968+polorei67@users.noreply.github.com>
2023-10-30 12:30:33 -0700

    Add files via upload

---
+++ main[1].py
@@ -0,0 +1,17 @@
+a = input("+ or - or * or /  :  ")
+
+b = input("say number one")
+
+c = input("say number two")
+
+if a == ("+"):
+  print(float(b) + float(c))
+
+elif a == ("-"):
+  print(float(b) - float(c))
+
+elif a == ("*"):
+  print(float(b) * float(c))
+
+elif a == ("/"):
+  print(float(b) / float(c))
